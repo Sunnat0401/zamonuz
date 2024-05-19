6e24d68afef70d8c174102d8e5a492c38532e8c6
sunnat <sunnattoxtaqulov4444@gmail.com>
2024-05-19 18:31:13 +0300

    about page has been created

--- src/Pages/HomePages/About/About.jsx
+++ src/Pages/HomePages/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './About.css'
 import Button from '../../../Component/Button/Button'
-import { train } from '../../../assets'
+import { airplane, train } from '../../../assets'
 const About = () => {
   return (
     <div className='about'>
@@ -14,12 +14,28 @@ const About = () => {
                 <div className="about-card">
                     <div className="about-info">
                         <div className="about-texts">
-                            <h3 className="about-subtitle">Temir Yo‘l Biletleri Biz Bilan</h3>
+                            <h2
+                             className="about-subtitle">Temir Yo‘l Biletlari Biz Bilan</h2>
                             <p className='about-paragraph'> Butun dunyo bo'ylab</p>
                         </div>
                         <Button width={'155px'} height={'47px'} bg={'#22B3C1'} color={'#fff'} borderRadius={'12px'} />
                     </div>
                     <p className='about-caption'>Temir yo'l orqali biz bilan sarguzashtlarni kashf eting! Baland tog'larni, betakror tabiat manzalarni, qadimiy va navqiron shaharlarni, takrorlanmas landshaflarni biz bilan birga toping. Qiziqarlimi? Biz bilan bog'laning.</p>
+                </div>
+            </div>
+            <div className="about-cards">
+                <img src={airplane} alt="train "  className='about-img'/>
+                <div className="about-card">
+                    <div className="about-info">
+                        <div className="about-texts">
+                            <h2
+                             className="about-subtitle">Aviabiletlar Biz Bilan Arzon</h2>
+                            <p className='about-paragraph'> Jahon bo‘ylab Manzillar
+</p>
+                        </div>
+                        <Button width={'155px'} height={'47px'} bg={'#22B3C1'} color={'#fff'} borderRadius={'12px'} />
+                    </div>
+                    <p className='about-caption'>Eng arzon, qulay, xavfsiz aviabiletlar biz bilan! Bizga bog'laning, biz sizga eng maqbul bo'ladigan aviabilerni topib beramiz</p>
                 </div>
             </div>
             </div>
