1188a606dc4f2dfc8438f627409aa1bfdc96e92e
sunnat <sunnattoxtaqulov4444@gmail.com>
2024-05-23 10:36:43 +0300

    slider pages finished pages

--- src/Pages/Slider/Slider.jsx
+++ src/Pages/Slider/Slider.jsx
@@ -1,72 +1,71 @@
-import React from 'react'
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import './Slider.css'
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-
-// import { EffectCoverflow, Pagination, Navigation } from 'swiper';
-import { dubai, India, sharm, usa } from '../../assets';
 import { EffectCoverflow, Navigation, Pagination } from 'swiper/modules';
+import { dubai, India, sharm, usa } from '../../assets';
+import './Slider.css';
+
 const Slider = () => {
   return (
     <div className="container">
-    <Swiper
-      effect={'coverflow'}
-      grabCursor={true}
-      centeredSlides={true}
-      loop={true}
-      slidesPerView={'auto'}
-      coverflowEffect={{
-        rotate: 0,
-        stretch: 0,
-        depth: 100,
-        modifier: 2.5,
-      }}
-      pagination={{ el: '.swiper-pagination', clickable: true }}
-      navigation={{
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-        clickable: true,
-      }}
-      modules={[EffectCoverflow, Pagination, Navigation]}
-      className="swiper_container"
-    >
-      <SwiperSlide>
-        <img src={India} alt="slide_image" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={usa} alt="slide_image" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={sharm} alt="slide_image" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={India} alt="slide_image" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={usa} alt="slide_image" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={dubai} alt="slide_image" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={sharm} alt="slide_image" />
-      </SwiperSlide>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        <SwiperSlide>
+          <img src={India} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={usa} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={sharm} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={dubai} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={India} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={usa} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={sharm} alt="slide_image" />
+        </SwiperSlide>
 
-      <div className="slider-controler">
-        <div className="swiper-button-prev slider-arrow">
-          <ion-icon  name="arrow-back-outline"></ion-icon>
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
         </div>
-        <div className="swiper-button-next slider-arrow">
-          <ion-icon name="arrow-forward-outline"></ion-icon>
-        </div>
-        <div className="swiper-pagination"></div>
-      </div>
-    </Swiper>
-  </div>
-  )
-}
+      </Swiper>
+    </div>
+  );
+};
 
-export default Slider
+export default Slider;
